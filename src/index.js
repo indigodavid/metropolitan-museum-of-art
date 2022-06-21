72b8b080777cdedeb826c2d0576ce2d0050af291
David Vera Castillo <indigodavid@gmail.com>
2022-06-20 22:02:21 -0500

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
 
-function component() {
+/* function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
   return element;
-}
+} */
 
 // document.body.appendChild(component());
