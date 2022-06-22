c9f2dceae963c739932c10f2af7ecc40781b48c1
David Vera Castillo <indigodavid@gmail.com>
2022-06-22 15:42:21 -0500

    Add module to add a new like

---
+++ src/modules/post-like.js
@@ -0,0 +1,18 @@
+const postLike = async (objectID, appID = 'pKSoTbGzFhj5RtoeFQif') => {
+  const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+  const endpoint = appID + '/likes/';
+  const response = await fetch(baseURL + endpoint, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: objectID,
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  },
+  );
+  const text = await response.text();
+  return text;
+}
+
+export default postLike;
