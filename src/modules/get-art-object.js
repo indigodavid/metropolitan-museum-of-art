19b4bfef8999ede0ef58c60d935534fbed3dd34b
David Vera Castillo <indigodavid@gmail.com>
2022-06-21 21:44:51 -0500

    Add module to get art object

---
+++ src/modules/get-art-object.js
@@ -0,0 +1,20 @@
+const getArtObject = async (objectId) => {
+  const baseURl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
+  const artObject = await fetch(baseURl + objectId);
+
+  const queryResult = await artObject.json();
+  if (
+    queryResult.title && 
+    queryResult.primaryImage && 
+    queryResult.primaryImageSmall && 
+    queryResult.artistDisplayName && 
+    queryResult.objectDate && 
+    queryResult.dimensions && 
+    queryResult.isPublicDomain
+  )
+  {
+    return queryResult;
+  }
+};
+
+export default getArtObject;
