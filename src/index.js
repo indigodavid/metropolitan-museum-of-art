b8db279ba014abf8475c85f3e21f8387e20c8798
David Vera Castillo <indigodavid@gmail.com>
2022-06-22 20:14:28 -0500

    Edit initial search as a variable to retrieve item count

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import getDepartments from './modules/get-departments.js';
 import getSearched from './modules/get-searched.js';
 import renderArtObject from './modules/render-art-object.js';
 import getArtObject from './modules/get-art-object.js';
+import itemCounter from './modules/item-counter';
+import renderItemCount from './modules/render-item-count';
 
 const departmentList = document.getElementById('department-list');
 const MAX_ART_OBJECTS = 12;
+const initialSearch = 'painting';
 
 getDepartments().then((array) => {
   array.forEach((element) => {
@@ -18,7 +21,7 @@ getDepartments().then((array) => {
   });
 });
 
-getSearched('painting').then((value) => {
+getSearched(initialSearch).then((value) => {
   for (let index = 0; index < 100; index += 1) {
     let counter;
     getArtObject(value[index]).then((artObject) => {
@@ -30,4 +33,8 @@ getSearched('painting').then((value) => {
       renderArtObject(artObject);
     });
   }
+
+  itemCounter(initialSearch).then(value => {
+    renderItemCount(value);
+  })
 });
