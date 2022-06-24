7dccb35eb82e2453ab76f245ba50b442a6be6861
David Vera Castillo <indigodavid@gmail.com>
2022-06-23 21:31:54 -0500

    Add new module to render arts in home

--- src/index.js
+++ src/index.js
@@ -3,27 +3,16 @@ import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
 import './desktop-style.css';
 import getSearched from './modules/get-searched.js';
-import renderArtObject from './modules/render-art-object.js';
-import getArtObject from './modules/get-art-object.js';
 import itemCounter from './modules/item-counter.js';
 import renderItemCount from './modules/render-item-count.js';
+import renderArtArray from './modules/render-art-array.js';
 
 const MAX_ART_OBJECTS = 12;
 const initialSearch = 'painting';
 
 getSearched(initialSearch).then((value) => {
-  for (let index = 0; index < 100; index += 1) {
-    let counter;
-    getArtObject(value[index]).then((artObject) => {
-      counter = document.getElementById('art-objects').children.length;
-      if (counter === MAX_ART_OBJECTS) {
-        index = Number.MAX_VALUE;
-        return;
-      }
-      renderArtObject(artObject);
-    });
-  }
-});
+  renderArtArray(value, MAX_ART_OBJECTS);
+})
 
 itemCounter(initialSearch).then((value) => {
   renderItemCount(value);
---
+++ src/modules/render-art-array.js
@@ -0,0 +1,17 @@
+import getArtObject from "./get-art-object.js";
+import renderArtObject from "./render-art-object";
+
+const renderArtArray = async (idsArray, size = 1) => {
+  const artArray = [];
+  let index = 0;
+  while (artArray.length < size ) {
+    const artObject = await getArtObject(idsArray[index++]);
+    if (artObject) {
+      artArray.push(artObject);
+      renderArtObject(artObject);
+    }
+  }
+  return artArray;
+}
+
+export default renderArtArray;
