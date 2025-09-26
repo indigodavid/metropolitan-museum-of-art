7089799432b1f8dbcf101c11f06dd970c74cf165
David Vera Castillo <indigodavid@gmail.com>
2025-09-26 09:08:08 -0500

    ✅ Add pagination behavior

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import getSearched from './modules/get-searched.js';
 import itemCounter from './modules/item-counter.js';
 import renderItemCount from './modules/render-item-count.js';
 import renderArtArray from './modules/render-art-array.js';
+import createPagination from './modules/pagination.js';
 
 const MAX_ART_OBJECTS = 12;
 const initialSearch = 'painting';
@@ -13,12 +14,44 @@ const searchButton = document.getElementById('search');
 const searchInput = document.getElementById('search-input');
 const artObjects = document.getElementById('art-objects');
 
-const searchArt = (query, displayedResults) => {
+let currentIdsArray = [];
+let currentPage = 1;
+let currentQuery = initialSearch;
+
+const renderPagination = (pageInfo) => {
+  const existingPagination = document.querySelector('.pagination');
+  if (existingPagination) {
+    existingPagination.remove();
+  }
+
+  if (pageInfo.totalPages > 1) {
+    const pagination = createPagination(
+      pageInfo.currentPage,
+      pageInfo.totalPages,
+      (newPage) => {
+        currentPage = newPage;
+        renderArtArray(currentIdsArray, currentPage, MAX_ART_OBJECTS)
+          .then(renderPagination);
+      },
+    );
+
+    artObjects.parentNode.insertBefore(pagination, artObjects.nextSibling);
+  }
+};
+
+const searchArt = (query, page = 1) => {
+  currentQuery = query;
+  currentPage = page;
+
   getSearched(query).then((value) => {
-    renderArtArray(value, displayedResults);
+    currentIdsArray = value;
+    return renderArtArray(value, page, MAX_ART_OBJECTS);
+  }).then((pageInfo) => {
+    renderPagination(pageInfo);
   }).catch((reason) => {
     artObjects.innerHTML = reason;
   });
+
   itemCounter(query).then((value) => {
     renderItemCount(value);
   }).catch(() => {
@@ -26,13 +59,13 @@ const searchArt = (query, displayedResults) => {
   });
 };
 
-searchArt(initialSearch, MAX_ART_OBJECTS);
+searchArt(initialSearch, 1);
 
 searchInput.oninput = () => {
   window.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
       if (searchInput.value) {
-        searchArt(searchInput.value, MAX_ART_OBJECTS);
+        searchArt(searchInput.value, 1);
         searchInput.value = '';
       }
     }
--- src/modules/render-art-array.js
+++ src/modules/render-art-array.js
@@ -1,28 +1,37 @@
 import getArtObject from './get-art-object.js';
 import renderArtObject from './render-art-object.js';
 
-const renderArtArray = async (idsArray, size = 1) => {
+const renderArtArray = async (idsArray, page = 1, pageSize = 12) => {
   const artArray = [];
-  let index = 0;
   const artObjects = document.getElementById('art-objects');
   artObjects.innerHTML = '';
-  if (!idsArray) {
+  if (!idsArray || idsArray.length === 0) {
     artObjects.innerHTML = '<h1>Not found</h1>';
     return [];
   }
-  while (artArray.length < size && index < idsArray.length) {
-    /* eslint-disable */
-    const artObject = await getArtObject(idsArray[index+=1]);
-    /* eslint-enable */
+
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = Math.min(startIndex + pageSize, idsArray.length);
+  const pageIds = idsArray.slice(startIndex, endIndex);
+
+  /* eslint-disable */
+  for (const id of pageIds) {
+    const artObject = await getArtObject(id);
     if (artObject) {
       artArray.push(artObject);
       renderArtObject(artObject);
     }
   }
+  /* eslint-enable */
   if (artArray.length === 0) {
     artObjects.innerHTML = '<h1>Sorry, the items could not be displayed</h1>';
   }
-  return artArray;
+  return {
+    artArray,
+    currentPage: page,
+    totalPages: Math.ceil(idsArray.length / pageSize),
+    totalItems: idsArray.length,
+  };
 };
 
 export default renderArtArray;
